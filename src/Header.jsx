db3b579c5e7a67ea8883b80c7b9b12a6033f4b45
labae <onetyten@gmail.com>
2024-09-20 16:38:15 +0100

    pxl

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header(props) {
   const{selectedTeam,teamMemberCount} = props
   return (
-    <div className='w-full top-8 sm:top-[72px] bg-dark py-1 sm:py-3 fixed '>
+    <div className='w-full top-[34px] sm:top-[72px] bg-dark py-1 sm:py-3 fixed '>
       <div className='flex justify-center w-full'>
         <h1 className='text-sm sm:text-xl text-white text-center border-white rounded-lg border-2 w-12'>
             {teamMemberCount}
