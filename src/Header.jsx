d26394a89f237bde21e87c65c810b1b6a183f5c7
labae <onetyten@gmail.com>
2024-09-20 16:11:39 +0100

    h adjust

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header(props) {
   const{selectedTeam,teamMemberCount} = props
   return (
-    <div className='w-full top-[75px] bg-dark py-3 fixed '>
+    <div className='w-full top-[72px] bg-dark py-3 fixed '>
       <div className='flex justify-center w-full'>
         <h1 className='text-xl sm:text-xl text-white text-center border-white rounded-lg border-2 w-12'>
             {teamMemberCount}
